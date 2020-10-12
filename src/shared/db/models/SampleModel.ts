b2d948a35dca580707a7e84b5586f91c8390490d
sfreeman422 <sfreeman422@protonmail.com>
2020-10-11 22:03:29 -0400

    Added uuid support to SampleModel

--- src/shared/db/models/SampleModel.ts
+++ src/shared/db/models/SampleModel.ts
@@ -2,8 +2,8 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class SampleModel {
-  @PrimaryGeneratedColumn()
-  public id!: number;
+  @PrimaryGeneratedColumn('uuid')
+  public id!: string;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   public createdAt!: Date;
