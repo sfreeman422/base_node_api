86dd0edad7644d284c3c05b3679cb0472d091516
sfreeman422 <sfreeman422@protonmail.com>
2022-12-31 17:09:13 -0500

    Updated all packages to latest, created new package-lock using node 18 and updated docker image to use node 18 image

--- src/index.ts
+++ src/index.ts
@@ -38,19 +38,7 @@ const connectToDb = async (): Promise<void> => {
 };
 
 const checkForEnvVariables = (): void => {
-  if (
-    !(
-      process.env.TYPEORM_CONNECTION &&
-      process.env.TYPEORM_HOST &&
-      process.env.TYPEORM_USERNAME &&
-      process.env.TYPEORM_PASSWORD &&
-      process.env.TYPEORM_DATABASE &&
-      process.env.TYPEORM_ENTITIES &&
-      process.env.TYPEORM_SYNCHRONIZE
-    )
-  ) {
-    throw new Error('Missing TYPEORM environment variables!');
-  }
+  // Update this section to check for relevant environment variables.
 };
 
 app.listen(PORT, (e?: Error) => {
