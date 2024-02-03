6d11cf8d2fd07e689dc7576a383baae2bb4ff415
sfreeman422 <sfreeman422@protonmail.com>
2024-02-03 10:49:31 -0500

    Updated example.env

--- src/shared/db/AppDataSource.ts
+++ src/shared/db/AppDataSource.ts
@@ -15,7 +15,7 @@ export const getDataSource = async (): Promise<DataSource> => {
     AppDataSource = new DataSource({
       type: process.env.type,
       host: process.env.host,
-      port: process.env.port,
+      port: process.env.dbport,
       username: process.env.username,
       password: process.env.password,
       database: process.env.database,
