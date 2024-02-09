2590de47e4095148c3930a505f3aaaa2d014cca3
sfreeman422 <sfreeman422@protonmail.com>
2024-02-09 10:30:12 -0500

    modified logic to use pg pool and removed typeorm references and libraries

---
+++ src/shared/db/DbPool.ts
@@ -0,0 +1,21 @@
+import { Logger } from '../services/logger/logger.service';
+import { Pool } from 'pg';
+
+export let dbPool: Pool;
+
+const logger = new Logger('DbPool');
+
+export const createPool = (): Pool => {
+  if (!dbPool) {
+    logger.info(`Creating Pool for ${process.env.database}@${process.env.host}:${process.env.dbport}`);
+    dbPool = new Pool({
+      user: process.env.username,
+      host: process.env.host,
+      database: process.env.database,
+      password: process.env.password,
+      port: parseInt(process.env.dbport as string),
+    });
+  }
+
+  return dbPool;
+};
